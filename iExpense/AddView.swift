78938a170821aeb281c611936ce904071b4df3fa
wtrieu <waylantrieu@gmail.com>
2024-02-04 10:25:07 -0800

    Set up base app.

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Waylan Trieu on 2/3/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
